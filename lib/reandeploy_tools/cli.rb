7d84e50358f650a8b4362c5806102da89401c110
Joe Khoobyar <joe@khoobyar.name>
2017-02-23 19:26:37 -0500

    various fixes

--- lib/reandeploy_tools/cli.rb
+++ lib/reandeploy_tools/cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'json'
+require 'pp'
 
 module REANDeployTools
   module Cli
@@ -10,7 +11,19 @@ module REANDeployTools
     class Base < Thor
       include Util
       
+      def self.inherited(base)
+        base.instance_eval do
+          class_option :config, default: REANDeployTools::Config.config_file, desc: "location of the reandeploy-tools config file"
+        end unless base.name =~ /::Main$/
+      end
+      
       private
+      
+      # FIXME: There should be a *documented* way to get notified by Thor about class options.
+      def options=(_options)
+        REANDeployTools::Config.config_file = _options['config'] if _options.include? 'config'
+        super
+      end
       
       def client
         ::REANDeployTools::Cli.client
