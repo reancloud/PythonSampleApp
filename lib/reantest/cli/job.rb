84f807794599cf0991dccfe813b43aa83e4ba536
Joe Khoobyar <joe@khoobyar.name>
2017-09-06 16:39:24 -0400

    CAV-58: URL tests work now

--- lib/reantest/cli/job.rb
+++ lib/reantest/cli/job.rb
@@ -7,9 +7,14 @@ require 'shellwords'
 module REANTest
   module Cli
     class Job < Base
+      DEFAULTS = {
+        browsers:           '{"Chrome":["53"]}',
+        run_crawl:          false,
+        page_load_timeout:  60,
+      }
 
-      option :job_config,  required: true,  desc: "filename to read input from, as JSON"
-      option :output,      required: true,  desc: "filename to write output to, as JSON"
+      option :job_config,  required: true,  desc: "filename to read job config from, as JSON"
+      option :output,      required: true,  desc: "filename to write job status to, as JSON"
       option :wait,        default: true, type: :boolean, desc: "Wait for the test execution to complete"
       option :reuse_vm,    type: :boolean,  desc: "reuse VM     (overrides \"executionStrategy\" in --job-config)"
       option :job_name,                     desc: "job name     (overrides \"appName\" in --job-config)"
@@ -18,36 +23,19 @@ module REANTest
       option :git_pass,                     desc: "git password (overrides \"gitPass\" in --job-config)"
       option :git_url,                      desc: "git URL      (overrides \"gitURL\" in --job-config)"
       option :git_branch,                   desc: "git branch   (overrides \"branchName\" in --job-config)"
-      desc "submit <TYPE>", "Run a cross-browser test job of the specified TYPE: functionaltest | urltest"
-      def submit(type)
-        raise "invalid test type: #{type}" unless type=='functionaltest' || type=='urltest'
+      desc "functionaltest", "Submits a cross-browser functional test job"
+      def functionaltest
         
         # Get the base job configuration, as JSON.
-        input = JSON.parse(File.read(options[:job_config]))
+        input = options[:job_config] ? JSON.parse(File.read(options[:job_config])) : {}
+        input['browsers'] ||= DEFAULTS[:browsers]
+        input['executionStrategy'] ||= 'boost'
           
-        # Allow parts of the configuration to be customized by other arguments.
-        input['type'] = type
-        input['appName'] = options[:job_name] if options[:job_name]
-        input['testURL'] = options[:test_url] if options[:test_url]
-        input['gitUser'] = options[:git_user] if options[:git_user]
-        input['gitPass'] = options[:git_pass] if options[:git_pass]
-        input['gitURL'] = options[:git_url] if options[:git_url]
-        input['branchName'] = options[:git_branch] if options[:git_branch]
-        case options[:reuse_vm]
-        when FalseClass
-          input['executionStrategy'] = 'boost'
-        when TrueClass
-          input['executionStrategy'] = 'vmReuse'
-        else
-          input['executionStrategy'] ||= 'boost'
-        end
-        
-        # Execute the testing job.
-        id = client.post "RunTest", input
-        die 'failed to run job' unless String===id && id.length > 0
+        # Submit the testing job.
+        id = submit_job 'functionaltest', input
         
         # Collect job status, possibly waiting until execution completes.
-        job = collect_status(id, 'functionaltest')
+        job = collect_status id, 'functionaltest'
         job['name'] = input['appName']
         
         # Output job status.
@@ -59,16 +47,53 @@ module REANTest
         exit 1 if options[:wait] && job['status'] != 'SUCCESS'
       end
 
-      option :output, required: true, desc: "filename to write output to, as JSON"
-      option :wait,   default: false, type: :boolean, desc: "Wait for the test execution to complete"
-      desc "status <ID>", "Get cross-browser test job status by ID"
-      def status id
+      option :job_config,  required: false, desc: "filename to read job config from, as JSON"
+      option :output,      required: true,  desc: "filename to write job status to, as JSON"
+      option :wait,        default: true, type: :boolean, desc: "Wait for the test execution to complete"
+      option :reuse_vm,    type: :boolean,        desc: "reuse VM           (overrides \"executionStrategy\" in --job-config)"
+      option :job_name,                           desc: "job name           (overrides \"appName\" in --job-config)"
+      option :test_url,                           desc: "test URL           (overrides \"testURL\" in --job-config)"
+      option :browsers,                           desc: "browsers JSON      (overrides \"browsers\" in --job-config)"
+      option :text_to_search,                     desc: "text to search     (overrides \"textToSearch\" in --job-config)"
+      option :run_crawl,   type: :boolean,        desc: "run crawl          (overrides \"runCrawl\" in --job-config)"
+      option :page_load_timeout, type: :numeric,  desc: "page load timeout  (overrides \"pageLoadTimeOut\" in --job-config)"
+      desc "urltest", "Submits a cross-browser URL test job"
+      def urltest
+        
+        # Get the base job configuration, as JSON.
+        input = options[:job_config] ? JSON.parse(File.read(options[:job_config])) : {}
+        input['executionStrategy'] ||= 'vmReuse'
+        input['runCrawl'] ||= DEFAULTS[:run_crawl]
+        input['browsers'] ||= DEFAULTS[:browsers]
+        input['pageLoadTimeOut'] ||= DEFAULTS[:page_load_timeout]
+          
+        # Submit the testing job.
+        id = submit_job 'urltest', input
+        
         # Collect job status, possibly waiting until execution completes.
-        job = collect_status(id)
-
+        job = collect_status id, 'urltest'
+        job['name'] = input['appName']
+        
         # Output job status.
         if output = options[:output]
           File.write(output, job.to_json)
+        end
+
+        # If we waited until the end, then fail if the job did not succeed.
+        exit 1 if options[:wait] && job['status'] != 'SUCCESS'
+      end
+
+      option :output, required: false, desc: "filename to write output to, as JSON"
+      option :wait,   default: false, type: :boolean, desc: "Wait for the test execution to complete"
+      desc "status <ID>", "Get cross-browser test job status by ID"
+      def status id
+        
+        # Collect job status, possibly waiting until execution completes.
+        job = collect_status id
+
+        # Output job status.
+        if output = options[:output]
+          File.write output, job.to_json
         end
 
         # Fail if the job did not succeed.
@@ -76,6 +101,31 @@ module REANTest
       end
       
       private
+      
+      # Submit a test to run and return a job ID.
+      def submit_job(type, input)
+        raise "invalid test type: #{type}" unless type=='functionaltest' || type=='urltest'
+          
+        # Allow parts of the configuration to be customized by other arguments.
+        input['type'] = type
+        input['appName'] = options[:job_name] if options[:job_name]
+        input['testURL'] = options[:test_url] if options[:test_url]
+        input['gitUser'] = options[:git_user] if options[:git_user]
+        input['gitPass'] = options[:git_pass] if options[:git_pass]
+        input['gitURL'] = options[:git_url] if options[:git_url]
+        input['branchName'] = options[:git_branch] if options[:git_branch]
+        input['browsers'] = options[:browsers] if options[:browsers]
+        input['textToSearch'] = options[:text_to_search] if options[:text_to_search]
+        input['runCrawl'] = options[:run_crawl] if options[:run_crawl]
+        input['pageLoadTimeOut'] = options[:page_load_timeout] if options[:page_load_timeout]
+        input['executionStrategy'] = (options[:reuse_vm] ? 'vmReuse' : 'boost') unless options[:reuse_vm].nil?
+        
+        # Execute the testing job.
+        id = client.post "RunTest", input
+        die 'failed to run job' unless String===id && id.length > 0
+        log "job #{type}: jobId #{id}"
+        id
+      end
       
       # Collect job status and possibly wait until completion.
       def collect_status(id, cmd='status')
@@ -92,7 +142,7 @@ module REANTest
           end
         end
         
-        job = {'id' => id, 'status' => job}
+        {'id' => id, 'status' => job}
       end
     end
   end
