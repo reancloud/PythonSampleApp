9819897362e3e5f89a6a6a7d88169ce277caed37
Joe Khoobyar <joe@khoobyar.name>
2017-09-06 16:01:19 -0400

    CAV-58: functional testing works

--- lib/reantest/cli/job.rb
+++ lib/reantest/cli/job.rb
@@ -11,32 +11,43 @@ module REANTest
       option :job_config,  required: true,  desc: "filename to read input from, as JSON"
       option :output,      required: true,  desc: "filename to write output to, as JSON"
       option :wait,        default: true, type: :boolean, desc: "Wait for the test execution to complete"
+      option :reuse_vm,    type: :boolean,  desc: "reuse VM     (overrides \"executionStrategy\" in --job-config)"
       option :job_name,                     desc: "job name     (overrides \"appName\" in --job-config)"
       option :test_url,                     desc: "test URL     (overrides \"testURL\" in --job-config)"
       option :git_user,                     desc: "git user     (overrides \"gitUser\" in --job-config)"
       option :git_pass,                     desc: "git password (overrides \"gitPass\" in --job-config)"
       option :git_url,                      desc: "git URL      (overrides \"gitURL\" in --job-config)"
       option :git_branch,                   desc: "git branch   (overrides \"branchName\" in --job-config)"
-      desc "runTest", "Run a cross-browser test job"
-      def runTest
+      desc "runTest <TYPE>", "Run a cross-browser test job of the specified TYPE: functionaltest | urltest"
+      def runTest(type)
+        raise "invalid test type: #{type}" unless type=='functionaltest' || type=='urltest'
         
         # Get the base job configuration, as JSON.
         input = JSON.parse(File.read(options[:job_config]))
           
         # Allow parts of the configuration to be customized by other arguments.
+        input['type'] = type
         input['appName'] = options[:job_name] if options[:job_name]
         input['testURL'] = options[:test_url] if options[:test_url]
         input['gitUser'] = options[:git_user] if options[:git_user]
         input['gitPass'] = options[:git_pass] if options[:git_pass]
         input['gitURL'] = options[:git_url] if options[:git_url]
         input['branchName'] = options[:git_branch] if options[:git_branch]
+        case options[:reuse_vm]
+        when FalseClass
+          input['executionStrategy'] = 'boost'
+        when TrueClass
+          input['executionStrategy'] = 'vmReuse'
+        else
+          input['executionStrategy'] ||= 'boost'
+        end
         
         # Execute the testing job.
         id = client.post "RunTest", input
         die 'failed to run job' unless String===id && id.length > 0
         
         # Collect job status, possibly waiting until execution completes.
-        job = collect_status(id, 'run')
+        job = collect_status(id, 'functionaltest')
         job['name'] = input['appName']
         
         # Output job status.
