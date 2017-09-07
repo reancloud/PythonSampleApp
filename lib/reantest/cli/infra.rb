4a55efbcaac593104c4143dcb67fd1dc9ab7de44
Joe Khoobyar <joe@khoobyar.name>
2017-09-07 10:06:40 -0400

    CAV-58: start to write infrastructure tests

--- lib/reantest/cli/infra.rb
+++ lib/reantest/cli/infra.rb
@@ -9,7 +9,47 @@ module REANTest
     class Infra < Base
       DEFAULTS = {
       }
+      
+      option :output,      required: true,  desc: "filename to write job status to, as JSON"
+      option :allow_unstable,  default: false, type: :boolean, desc: "Partial success (UNSTABLE) is acceptable"
+      option :wait,        default: true, type: :boolean, desc: "Wait for the test execution to complete"
+      option :job_name,                     desc: "job name"
+      option :region,                       desc: "AWS region"
+      option :aws_secret_access_key,        desc: "AWS secret access key"
+      option :aws_access_key_id,            desc: "AWS access key id"
+      option :input_param,                  desc: "filename to read input parameter from, as JSON"
+      option :output_param,                 desc: "filename to read output parameter from, as JSON"
+      desc "autotest", "Submits a REAN-defined infrastructure test job"
+      def autotest
+        input = {}
+          
+        # Allow parts of the configuration to be customized by other arguments.
+        input['name'] = options[:job_name] if options[:job_name]
+        input['region'] = options[:region] if options[:region]
+        input['secreteKey'] = options[:aws_secret_acces_key] if options[:aws_secret_acces_key]
+        input['accessKey'] = options[:aws_access_key_id] if options[:aws_access_key_id]
+        input['input'] = File.read(options[:inputParam]) if options[:inputParam]
+        input['output'] = File.read(options[:outputParam]) if options[:outputParam]
+        
+        # Execute the testing job.
+        id = client.post "infratest/awspec", input
+        die 'failed to run job' unless String===id && id.length > 0
+        log "infra autotest: jobId #{id}"
+        id
+        
+        # Collect job status, possibly waiting until execution completes.
+        job = collect_status id, 'autotest'
+        job['name'] = input['name']
+        
+        # Output job status.
+        if output = options[:output]
+          File.write(output, job.to_json)
+        end
 
+        # If we waited until the end, then fail if the job did not succeed.
+        exit 1 if options[:wait] && job['status'] != 'SUCCESS' && job['status'] != 'UNSTABLE'
+      end
+      
       option :output, required: false, desc: "filename to write output to, as JSON"
       option :allow_unstable,  default: false, type: :boolean, desc: "Partial success (UNSTABLE) is acceptable"
       option :wait,   default: false, type: :boolean, desc: "Wait for the test execution to complete"
