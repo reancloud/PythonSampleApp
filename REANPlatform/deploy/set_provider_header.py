f12b2a1a7832490cc1c145d46b3caa063ff6ed5e
patilmayuri <mayuri.patil@reancloud.com>
2018-05-15 18:26:36 +0530

    pydocstyle changes

--- REANPlatform/deploy/set_provider_header.py
+++ REANPlatform/deploy/set_provider_header.py
@@ -1,9 +1,11 @@
+"""Provider set_header module."""
 import deploy_sdk_client
 from deploy_sdk_client.rest import ApiException
 from deploy.constants import Constants
 
 
 def set_header():
+    """Set header to provider API call."""
     provider_api_instance = deploy_sdk_client.ProviderApi()
     provider_api_instance.api_client.set_default_header(
                     Constants.AUTHORIZATION,
