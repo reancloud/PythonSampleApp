8030cc8769749f14ef90cd49fa6ef258a2e3883e
ihiteshri12 <hiteshri.ramteke>
2018-06-14 16:57:02 +0530

    Deploy Env code updated with latest API

--- REANPlatform/deploy/deployenv.py
+++ REANPlatform/deploy/deployenv.py
@@ -1,8 +1,9 @@
-"""Depoly Environment."""
+"""Depoly/Re-Deploy an Environment."""
 import os
 import re
-from pprint import pprint
+import os.path
 import logging
+import json
 from cliff.command import Command
 import deploy_sdk_client
 from deploy_sdk_client.rest import ApiException
@@ -11,147 +12,96 @@ from reanplatform.utility import Utility
 
 
 class DepolyEnv(Command):
-    """Depoly Environment."""
+    """Redeploy an environment by name and version."""
 
     log = logging.getLogger(__name__)
 
     def get_parser(self, prog_name):
         """get_parser."""
-        # Define parser
         parser = super(DepolyEnv, self).get_parser(prog_name)
-        try:
-            parser.add_argument('--env_name', '-ename',
-                                help='Environment name, This parameter is \
-                                not required when environmentId is specified',
-                                required=False)
-            parser.add_argument('--env_version', '-env_v',
-                                help='Environment version. This parameter is \
-                                not required when environmentId is specified',
-                                required=False)
-            parser.add_argument('--deployment_name', '-dname',
-                                default='default',
-                                help='Deployment Name',
-                                required=False)
-            parser.add_argument('--env_id', '-id',
-                                help='Environment Id',
-                                required=False)
-            parser.add_argument('--deployment_description', '-desc',
-                                help='Deployment Description',
-                                required=False)
-            parser.add_argument('--provider_name', '-pname',
-                                help='Provider Name',
-                                required=False)
-            parser.add_argument('--region', '-region',
-                                help='Region Name',
-                                required=False)
-            parser.add_argument('--env_version_id', '-env-ver-id',
-                                help='Environment Version ID',
-                                required=False)
-            parser.add_argument('--json_file', '-j-file',
-                                help='Pass input variable json file with \
-                                full path',
-                                required=False)
-            parser.add_argument('--json_str', '-j-str',
-                                help='Pass input variables in a string \
-                                like \'{"Key": "Value"}\'',
-                                required=False)
+        parser.add_argument('--env_id', '-id',
+                            help='Environment id',
+                            required=True)
+        parser.add_argument('--deployment_name', '-dname', default='default',
+                            help='Deployment Name. Please provide this \
+                            attribute if deployment name is not default.',
+                            required=False)
+        parser.add_argument('--deployment_description', '-desc',
+                            help='Deployment Description',
+                            required=False)
+        parser.add_argument('--provider_name', '-pname',
+                            help='Provider Name',
+                            required=False)
+        parser.add_argument('--region', '-region',
+                            help='Region Name',
+                            required=False)
+        parser.add_argument('--env_version_id', '-env-ver-id',
+                            help='Please provide this attribute only if you \
+                            want to upgrade existing deployment with newer \
+                            version of an environment. A value should be the \
+                            version of same environment',
+                            required=False)
+        parser.add_argument('--json_file', '-json',
+                            help='Pass input variable json file with \
+                            full path',
+                            required=False)
 
-            return parser
-        except ApiException as e:
-            Utility.print_exception(e)
+        return parser
 
-    def validate(self, env_id, env_name, env_version, json_file, json_str):
-        """Valiate parsed arguments."""
-        if env_id and env_name and env_version:
-            message = "Please provide either Environment's ID or Name \
-            and Version."
-            exception_msg = re.sub(' +', ' ', message)
-            raise Exception(exception_msg)
-        elif env_id and env_name:
-            message = "Please provide either Environment's ID or Name \
-            and Version. Do not provide Environment ID and Name both."
-            exception_msg = re.sub(' +', ' ', message)
-            raise Exception(exception_msg)
-        elif env_id and env_version:
-            message = "Please provide either Environment's ID or Name \
-            and Version. Do not provide Environment ID and Version both."
-        if json_file and json_str:
-            message = "Please provide Input Variable json either in the \
-            form of file or in the form if string."
-            exception_msg = re.sub(' +', ' ', message)
-            raise Exception(exception_msg)
-
-    def convert_json_to_string(self, json_file):
-        """Convert Json into String."""
+    @staticmethod
+    def pass_json_as_object(json_file):
+        """Convert Json to String."""
         try:
             # check file exists
             if os.path.isfile(json_file) is False:
                 print('File not found: ' + json_file)
 
             # get a file object and read it in as a string
-            fileobj = open(json_file)
-            jsonstr = fileobj.read()
-            fileobj.close()
-            return jsonstr
-
+            with open(json_file) as jsonfile:
+                json_obj = json.load(jsonfile)
+            return json_obj
         except ApiException as e:
             Utility.print_exception(e)
 
-    def deploy_env(self, deployment_name, deployment_description, region,
-                   provider_name, input_json, env_id, env_name, env_version):
-        """Deploy Environment."""
-        # Initialise instance and api_instance
-        instance = deploy_sdk_client.EnvironmentApi()
-        api_instance = set_header_parameter(instance)
-        # Prepare deployment configuration body
-        body = deploy_sdk_client.DeploymentConfiguration(
-            deployment_name=deployment_name,
-            deployment_description=deployment_description,
-            region=region,
-            provider_name=provider_name,
-            input_json=input_json
-        )
-        # Deploy Environment By ID
-        if env_id:
-            try:
-                api_response = api_instance.deploy(env_id, body=body)
-                pprint(api_response)
-            except ApiException as e:
-                Utility.print_exception(e)
-        # Deploy Environment By Name And Version
-        elif env_name and env_version:
-            try:
-                api_response = api_instance.deploy_0(
-                    env_name,
-                    env_version,
-                    body=body
-                )
-                pprint(api_response)
-            except ApiException as e:
-                Utility.print_exception(e)
+    @staticmethod
+    def re_deploy_environment(environment_id, deployment_name,
+                              deployment_description, provider_name,
+                              input_json, region):
+        """Redeploy An Environment."""
+        try:
+            # Initialise instance and api_instance
+            instance = deploy_sdk_client.EnvironmentApi()
+            api_instance = set_header_parameter(instance)
+            body = deploy_sdk_client.DeploymentConfiguration(
+                environment_id=environment_id,
+                deployment_name=deployment_name,
+                deployment_description=deployment_description,
+                region=region,
+                provider_name=provider_name,
+                input_json=input_json
+            )
+            api_response = api_instance.deploy_1(
+                body=body
+            )
+            print(api_response)
+        except ApiException as e:
+            Utility.print_exception(e)
 
     def take_action(self, parsed_args):
-        """Deploy Environment Action."""
-        # Define parsed arguments
-        env_id = parsed_args.env_id
+        """take_action."""
+        # Define parsed_args
+        environment_id = parsed_args.env_id
         deployment_name = parsed_args.deployment_name
         deployment_description = parsed_args.deployment_description
         region = parsed_args.region
-        provider_name = parsed_args.provider_name
         json_file = parsed_args.json_file
-        json_str = parsed_args.json_str
-        env_name = parsed_args.env_name
-        env_version = parsed_args.env_version
+        provider_name = parsed_args.provider_name
+        input_json = None
 
-        # Valiate parsed arguments
-        self.validate(env_id, env_name, env_version, json_file, json_str)
+        input_json = DepolyEnv.pass_json_as_object(json_file)
+        print(input_json)
 
-        # Set json format
-        if json_str:
-            input_json = json_str
-        elif json_file:
-            input_json = self.convert_json_to_string(json_file)
-
-        self.deploy_env(deployment_name, deployment_description, region,
-                        provider_name, input_json, env_id, env_name,
-                        env_version)
+        # Re Deploy an environment
+        DepolyEnv.re_deploy_environment(environment_id, deployment_name,
+                                        deployment_description, provider_name,
+                                        input_json, region)
