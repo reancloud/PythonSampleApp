8d3550da79273a9c372f9d58685dee2ce96430e5
ihiteshri12 <hiteshri.ramteke>
2018-06-15 11:42:07 +0530

    started working on Rule Install CLI

--- REANPlatform/mnc/rule_install.py
+++ REANPlatform/mnc/rule_install.py
@@ -1,5 +1,6 @@
 import logging
 from cliff.command import Command
+from 
 
 
 class RuleInstall(Command):
@@ -9,23 +10,30 @@ class RuleInstall(Command):
 
     def get_parser(self, prog_name):
         parser = super(RuleInstall, self).get_parser(prog_name)
-
-        parser.add_argument(
-            '--rule-name', help='Set the rule name for installation', action="append", nargs='*', required=False)
-        parser.add_argument(
-            '--rule-type', help='Set the rule type for installation', action="append", nargs='*', required=False)
-        parser.add_argument(
-            '--customer-acc', help='Set the customer account number', action="store", required=True, type=int)
-        parser.add_argument(
-            '--deploy-provider', help='Set the REANDeploy provider for customer', action="store", required=False)
-        parser.add_argument(
-            '--customer-email-to', help='Set the TO customer email', action="append", nargs='*', required=False)
-        parser.add_argument(
-            '--customer-email-cc', help='Set the CC customer email', action="append", nargs='*', required=False)
-        parser.add_argument(
-            '--customer-email-domain', help='Set the customer email domain', action="store", required=False)
-        parser.add_argument(
-            '--action', help='Set the action', action="store_true", required=False)
+        parser.add_argument('--rule_name', '-name',
+                            help='Set the rule name for installation',
+                            required=True)
+        parser.add_argument('--rule_type', '-type',
+                            help='Set the rule type for installation',
+                            required=False)
+        parser.add_argument('--customer_acc', '-acc',
+                            help='Set the customer account number',
+                            required=True, type=int)
+        parser.add_argument('--deploy-provider', '-p',
+                            help='Set the REANDeploy provider for customer',
+                            required=False)
+        parser.add_argument('--customer_email_to', '-email_to',
+                            help='Set the TO customer email',
+                            required=False)
+        parser.add_argument('--customer-email-cc', '-email_cc',
+                            help='Set the CC customer email',
+                            required=False)
+        parser.add_argument('--customer-email-domain', '-email_domain',
+                            help='Set the customer email domain',
+                            required=False)
+        parser.add_argument('--action',
+                            help='Set the action',
+                            required=False)
 
         return parser
 
