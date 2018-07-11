ab291a86e975e9bd5dee836a030a3dc6830572e4
Priyanka <priyanka.khairnar@reancloud.com>
2018-07-11 13:44:16 +0530

    Implemented review comments

--- REANPlatform/auth/get_users.py
+++ REANPlatform/auth/get_users.py
@@ -29,38 +29,31 @@ class GetUsers(Command):
     def get_users(output_format):
         """Get users."""
         try:
-            # Initialise instance and api_instance in list_environment
+            # Initialise instance and api_instance in list_user
             instance = authnz_sdk_client.UsercontrollerApi()
             api_instance = set_header_parameter(instance, Utility.get_url(AunthnzConstants.AUTHNZ_URL))
-            # Get all environments for user
+            # Get all users for user
             api_response = api_instance.get_all_user_using_get()
 
             if output_format == 'table':
-                table = PrettyTable(['Id', 'Name', 'UserName', 'Email', 'Verified', 'Disabled', 'Verification Date', 'Verification Id'])
+                table = PrettyTable(['Id', 'Name', 'Username', 'Email', 'Verified', 'Disabled'])
                 table.padding_width = 1
-                for environment in api_response:
+                for user in api_response:
                     table.add_row(
                         [
-                            environment.id,
-                            environment.name,
-                            environment.username,
-                            environment.email,
-                            environment.verified,
-                            environment.disabled,
-                            environment.verification_date,
-                            environment.verification_id
+                            user.id,
+                            user.name,
+                            user.username,
+                            user.email,
+                            user.verified,
+                            user.disabled
                         ]
                     )
                 print("Users list ::\n%s" % (table))
 
             elif output_format == 'json' or output_format == '':
-                print(
-                    json.dumps(
-                        api_response,
-                        default=lambda o: o.__dict__,
-                        sort_keys=True, indent=4
-                        ).replace("\"_", '"')
-                    )
+                parsed_response = GetUsers.parse_response(api_response)
+                print(parsed_response)
 
         except ApiException as e:
             Utility.print_exception(e)
@@ -72,3 +65,26 @@ class GetUsers(Command):
 
         # List Users
         GetUsers.get_users(output_format)
+
+    @staticmethod
+    def parse_response(api_response):
+        """Parse api response."""
+        json_response = {}
+        user_list = []
+        for user in api_response:
+            json_response = {
+                'id' : user.id,
+                'name': user.name,
+                'username': user.username,
+                'email': user.email,
+                'verified': user.verified,
+                'disabled': user.disabled
+            }
+            user_list.append(json_response)
+
+        parsed_response = json.dumps(
+            user_list,
+            default=lambda o: o.__dict__,
+            sort_keys=True, indent=4
+        ).replace("\"_", '"')
+        return parsed_response
