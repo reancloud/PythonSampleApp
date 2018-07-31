2f71c95563792bbd2d768cdc7203af3d9a3702da
patilmayuri <mayuri.patil@reancloud.com>
2018-07-31 12:27:19 +0530

    List formate updated

--- REANPlatform/mnc/rule_list.py
+++ REANPlatform/mnc/rule_list.py
@@ -2,6 +2,8 @@
 import logging
 import json
 import re
+import time
+import ast
 from collections import OrderedDict
 from cliff.command import Command
 from mnc.parameters_constants import MncConstats
@@ -10,6 +12,7 @@ from deploy_sdk_client.rest import ApiException
 from reanplatform.utility import Utility
 from reanplatform.set_header import set_header_parameter
 from deploy.constants import DeployConstants
+from prettytable import PrettyTable
 
 
 class RuleList(Command):        # noqa: D203.
@@ -21,20 +24,17 @@ class RuleList(Command):        # noqa: D203.
         """get_parser."""
         parser = super(RuleList, self).get_parser(prog_name)
         parser.add_argument('--' + MncConstats.RULE_NAME, MncConstats.RULE_NAME_INITIAL,
-                            help='Rule name',
-                            required=False)
-        parser.add_argument('--' + MncConstats.RULE_TYPE, MncConstats.RULE_TYPE_INITIAL,
-                            help='Rule type',
+                            help='Rule name.',
                             required=False)
         parser.add_argument('--' + MncConstats.CUSTOMER_ACC, MncConstats.CUSTOMER_ACC_INITIAL,
-                            help='Customer account number',
+                            help='Customer account number.',
                             required=False)
         return parser
 
     # pylint: disable=R0201
-    def __validate_parameters(self, rule_name, rule_type, customer_acc):
+    def __validate_parameters(self, rule_name, customer_acc):
         """Validate cli parameters."""
-        if rule_name is None and rule_type is None and customer_acc is None:
+        if rule_name is None and customer_acc is None:
             raise RuntimeError("Specify either " + '--' + MncConstats.RULE_NAME + " OR " + '--' + MncConstats.CUSTOMER_ACC + " OR " + '--' + MncConstats.RULE_NAME + " and " + '--' + MncConstats.CUSTOMER_ACC)
 
     def take_action(self, parsed_args):
@@ -42,51 +42,60 @@ class RuleList(Command):        # noqa: D203.
         argparse_dict = vars(parsed_args)
 
         rule_name = argparse_dict[MncConstats.RULE_NAME]
-        rule_type = argparse_dict[MncConstats.RULE_TYPE]
         customer_acc = argparse_dict[MncConstats.CUSTOMER_ACC]
         rule_name_key = None
-        self.__validate_parameters(rule_name, rule_type, customer_acc)
+        self.__validate_parameters(rule_name, customer_acc)
         try:
             instance = deploy_sdk_client.EnvironmentApi()
             api_instance = set_header_parameter(instance, Utility.get_url(DeployConstants.DEPLOY_URL))
             all_env = api_instance.get_all_environments()
-
+            logging.info("Please wait!")
             display_data = OrderedDict()
+            table = None
+            table_only_acc = PrettyTable(['Customer Account Number', 'Rule Name'])
+            table_only_name = PrettyTable(['Rule Name', ' Customer Account Number'])
+            add_acc = True
             for one_env in all_env:
                 input_json = None
                 if rule_name and customer_acc:
                     if one_env.name.startswith(rule_name):
                         input_json = RuleList.get_input_json(one_env.config.env_id, customer_acc, api_instance, rule_name=None)
                         if input_json:
+                            input_json = ast.literal_eval(str(input_json))
                             display_data['Customer-Account'] = customer_acc
                             display_data['Rule-name'] = rule_name
                             display_data['Rule-type'] = None
                             display_data['Input-Parameters'] = input_json
                 elif customer_acc:
                     input_json = RuleList.get_input_json(one_env.config.env_id, customer_acc, api_instance, rule_name)
-                    rule_name = ''
                     if input_json:
-                        display_data['Customer-Account'] = customer_acc
-                        display_data['Rule-type'] = rule_type
-                        rule_name_key = one_env.name.replace('_config_rule_setup', '')
-                        display_data[rule_name_key] = {
-                            "Input-Parameters": input_json}
+                        rule_name_add = one_env.name.replace('_config_rule_setup', '')
+                        customer_acc = customer_acc if add_acc else ''
+                        table_only_acc.add_row([customer_acc, rule_name_add])
+                        add_acc = False
+                        table = table_only_acc
                 elif rule_name:
                     if one_env.name.startswith(rule_name):
                         input_all_deployment = RuleList.get_input_json(one_env.config.env_id, customer_acc, api_instance, rule_name)
-
                         if input_all_deployment:
-                            display_data['Rule-Name'] = rule_name
-                            display_data['Rule-type'] = rule_type
+                            add_acc = True
                             for input_json in input_all_deployment:
                                 if input_json['input_data'] and ''.join(input_json['acc_no']):
-                                    display_data[''.join(input_json['acc_no'])] = {
-                                        "Input-Parameters": input_json['input_data']}
-            if display_data:
-                logging.info(json.dumps(display_data, indent=4))
+                                    rule_name = rule_name if add_acc else ''
+                                    table_only_name.add_row([
+                                        rule_name,
+                                        ''.join(input_json['acc_no'])
+                                    ])
+                                    add_acc = False
+                    table = table_only_name
+            if table:
+                logging.info(table)
+            elif display_data:
+                logging.info(json.dumps(display_data, default=lambda o: o.__dict__, sort_keys=False, indent=4).replace("\"_", '"'))
             else:
-                logging.info("Rule deployment not found")
+                logging.info("No rule deployment found.")
         except ApiException as exception:
+            logging.info("Please try again.")
             Utility.print_exception(exception)
 
     @staticmethod
@@ -96,11 +105,12 @@ class RuleList(Command):        # noqa: D203.
         input_data = None
         input_all_deployment = []
         all_deployment = api_instance.get_all_deployments_for_environment_by_id(env_id)
-
+        time.sleep(2)
         if all_deployment:
             for single_deployment in all_deployment:
                 if customer_acc and customer_acc in single_deployment.deployment_name or rule_name:
                     input_data = api_instance.get_deployment_input_json(env_id=env_id, deployment_name=single_deployment.deployment_name)
+                    time.sleep(2)
                     if rule_name and 'client' in single_deployment.deployment_name:
                         data = {}
                         data = {
