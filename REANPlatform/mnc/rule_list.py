060d3b66f61b08fb6be6145ce13ad046568216f6
patilmayuri <mayuri.patil@reancloud.com>
2018-07-31 13:53:03 +0530

    status added

--- REANPlatform/mnc/rule_list.py
+++ REANPlatform/mnc/rule_list.py
@@ -12,6 +12,7 @@ from deploy_sdk_client.rest import ApiException
 from reanplatform.utility import Utility
 from reanplatform.set_header import set_header_parameter
 from deploy.constants import DeployConstants
+from deploy.getdeploymentstatus import Status
 from prettytable import PrettyTable
 
 
@@ -52,8 +53,8 @@ class RuleList(Command):        # noqa: D203.
             logging.info("Please wait!")
             display_data = OrderedDict()
             table = None
-            table_only_acc = PrettyTable(['Customer Account Number', 'Rule Name'])
-            table_only_name = PrettyTable(['Rule Name', ' Customer Account Number'])
+            table_only_acc = PrettyTable(['Customer Account Number', 'Rule Name', 'Status'])
+            table_only_name = PrettyTable(['Rule Name', 'Customer Account Number', 'Status'])
             add_acc = True
             for one_env in all_env:
                 input_json = None
@@ -61,7 +62,7 @@ class RuleList(Command):        # noqa: D203.
                     if one_env.name.startswith(rule_name):
                         input_json = RuleList.get_input_json(one_env.config.env_id, customer_acc, api_instance, rule_name=None)
                         if input_json:
-                            input_json = ast.literal_eval(str(input_json))
+                            del input_json['status']
                             display_data['Customer-Account'] = customer_acc
                             display_data['Rule-name'] = rule_name
                             display_data['Rule-type'] = None
@@ -71,7 +72,7 @@ class RuleList(Command):        # noqa: D203.
                     if input_json:
                         rule_name_add = one_env.name.replace('_config_rule_setup', '')
                         customer_acc = customer_acc if add_acc else ''
-                        table_only_acc.add_row([customer_acc, rule_name_add])
+                        table_only_acc.add_row([customer_acc, rule_name_add, input_json['status']])
                         add_acc = False
                         table = table_only_acc
                 elif rule_name:
@@ -84,7 +85,8 @@ class RuleList(Command):        # noqa: D203.
                                     rule_name = rule_name if add_acc else ''
                                     table_only_name.add_row([
                                         rule_name,
-                                        ''.join(input_json['acc_no'])
+                                        ''.join(input_json['acc_no']),
+                                        input_json['status']
                                     ])
                                     add_acc = False
                     table = table_only_name
@@ -110,16 +112,20 @@ class RuleList(Command):        # noqa: D203.
             for single_deployment in all_deployment:
                 if customer_acc and customer_acc in single_deployment.deployment_name or rule_name:
                     input_data = api_instance.get_deployment_input_json(env_id=env_id, deployment_name=single_deployment.deployment_name)
+                    input_data = ast.literal_eval(str(input_data))
+                    status = Status.deployment_status(env_id, single_deployment.deployment_name)
                     time.sleep(2)
                     if rule_name and 'client' in single_deployment.deployment_name:
                         data = {}
                         data = {
                             'deployment_name': single_deployment.deployment_name,
                             'input_data': input_data,
-                            'acc_no': re.findall(r'-?\d+\.?\d*', single_deployment.deployment_name)
+                            'acc_no': re.findall(r'-?\d+\.?\d*', single_deployment.deployment_name),
+                            'status': status
                         }
                         input_all_deployment.append(data)
                     else:   # for acc num
                         if input_data and 'client' in single_deployment.deployment_name:
                             input_all_deployment = input_data
+                            input_data['status'] = status
         return input_all_deployment
