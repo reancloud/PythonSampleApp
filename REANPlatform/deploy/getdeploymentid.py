c42ad32a2775c538dde2320f56e1879ad6e16027
ShantanuRean <shantanu.parate@reancloud.com>
2018-08-21 15:53:19 +0530

    updated get-deployment-id command output

--- REANPlatform/deploy/getdeploymentid.py
+++ REANPlatform/deploy/getdeploymentid.py
@@ -1,5 +1,4 @@
 """Get Deployment ID."""
-from pprint import pprint
 import logging
 from cliff.command import Command
 import deploy_sdk_client
@@ -11,7 +10,7 @@ from deploy.utility import DeployUtility
 
 
 class GetDeployments(Command):
-    """Get Deployments."""
+    """Get Deployment Id."""
 
     log = logging.getLogger(__name__)
 
@@ -19,41 +18,19 @@ class GetDeployments(Command):
         """get_parser."""
         # Define parser
         parser = super(GetDeployments, self).get_parser(prog_name)
-        parser.add_argument('--deployment_name', '-n',
-                            help='Deployment Name. Provide this attribute \
-                                to get specific deployment.',
-                            required=False)
-        parser.add_argument('--env_id', '-id',
-                            help='Environment Id',
-                            required=True)
+        parser.add_argument('--deployment_name', '-n', default='default', help='Deployment name. Provide this attribute to get specific deployment else deployment name will be default', required=False)
+        parser.add_argument('--env_id', '-i', help='Environment id', required=True)
         return parser
-
-    @staticmethod
-    def get_deployments_by_id(env_id):
-        """Get Deployments By Env ID."""
-        try:
-            # Initialise instance and api_instance
-            api_client = set_header_parameter(DeployUtility.create_api_client(), Utility.get_url(DeployConstants.DEPLOY_URL))
-            instance = deploy_sdk_client.EnvironmentApi(api_client)
-            res = instance.get_all_deployments_for_environment_by_id(
-                env_id
-            )
-            pprint(res)
-        except ApiException as api_exception:
-            Utility.print_exception(api_exception)
 
     @staticmethod
     def get_deployment_by_id_and_name(env_id, deployment_name):
         """Get Deployments by Env ID And Deployment Name."""
         try:
-            # Initialise instance and api_instance
+            # Initialise api_client and api_instance
             api_client = set_header_parameter(DeployUtility.create_api_client(), Utility.get_url(DeployConstants.DEPLOY_URL))
-            instance = deploy_sdk_client.EnvironmentApi(api_client)
-            res = instance.get_all_deployments_for_environment_by_id_and_deployment_name(
-                env_id,
-                deployment_name
-            )
-            pprint(res)
+            api_instance = deploy_sdk_client.EnvironmentApi(api_client)
+            api_response = api_instance.get_all_deployments_for_environment_by_id_and_deployment_name(env_id, deployment_name)
+            print("Deployment id : %s " % (api_response.id))
         except ApiException as api_exception:
             Utility.print_exception(api_exception)
 
@@ -63,7 +40,5 @@ class GetDeployments(Command):
         env_id = parsed_args.env_id
         deployment_name = parsed_args.deployment_name
 
-        if env_id and deployment_name:
-            GetDeployments.get_deployment_by_id_and_name(env_id, deployment_name)  # noqa: E501
-        elif env_id:
-            GetDeployments.get_deployments_by_id(env_id)
+        if env_id:
+            GetDeployments.get_deployment_by_id_and_name(env_id, deployment_name)
