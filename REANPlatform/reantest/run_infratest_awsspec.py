e9dc9fff97f4ef64b0d38513a5a3407af5b9c098
Mayur Patil <mayur.patil@reancloud.com>
2018-09-10 14:45:15 +0530

    Infra Test AWS Spec CLI implementation

---
+++ REANPlatform/reantest/run_infratest_awsspec.py
@@ -0,0 +1,72 @@
+"""Run Infratest AWSSpec."""
+from cliff.command import Command
+import logging
+import json
+from reantest.utility import Utility
+import test_sdk_client
+
+
+class RunInfraTestAwsSpec(Command):
+    """Run Infratest AWSSpec."""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        """get_parser."""
+        parser = super(RunInfraTestAwsSpec, self).get_parser(prog_name)
+
+        parser.add_argument('-application_name', '-a', help='Set the name for this Infra test Job', required=True)
+        parser.add_argument('-provider', '-pf', help='Provide file aws provider json file path', required=True)
+        parser.add_argument('-input', '-i', help='Input json file', required=True)
+        parser.add_argument('-output', '-o', help='Output json file', required=True)
+        return parser
+
+    def take_action(self, parsed_args):
+        """take_action."""
+        try:
+
+            self.log.debug(parsed_args)
+
+            body = test_sdk_client.AwspecParam()
+            body.name = parsed_args.application_name
+            aws_provider = test_sdk_client.AwsProvider()
+            with Utility.open_file(parsed_args.provider) as handle:
+                filedata = handle.read()
+
+            provider_json = json.loads(filedata)
+
+            aws_provider.access_key = provider_json['access_key']
+            aws_provider.secret_key = provider_json['secret_key']
+            aws_provider.region = provider_json['region']
+
+            if provider_json.get('iam_instance_profile') is not None:
+                instance_profile = test_sdk_client.InstanceProfile
+                instance_profile.name = provider_json.iam_instance_profile['name']
+                instance_profile.arn = provider_json.iam_instance_profile['arn']
+                aws_provider.iam_instance_profile = instance_profile
+
+            if provider_json.get('assume_role') is not None:
+                assume_role = test_sdk_client.AssumeRole
+                assume_role.role_arn = provider_json.assume_role['role_arn']
+                assume_role.session_name = provider_json.assume_role['session_name']
+                assume_role.external_id = provider_json.assume_role['external_id']
+                aws_provider.assume_role = assume_role
+
+            body.provider = aws_provider
+
+            with Utility.open_file(parsed_args.input) as handle:
+                input_data = handle.read()
+
+            body.input = json.loads(input_data)
+
+            with Utility.open_file(parsed_args.input) as handle:
+                output_data = handle.read()
+
+            body.output = json.loads(output_data)
+
+            self.log.debug(body)
+            self.log.debug("Execution stared for RunInfraTestAwsSpec")
+            Utility.execute_test(body, parsed_args, self.log, test_sdk_client.InfraTestApi(Utility.set_headers()).execute_infra_awspec)
+
+        except Exception as exception:
+            Utility.print_exception(exception)
