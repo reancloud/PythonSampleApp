b440ceb0e414d17a2eacd1729064d1062a343db3
ShantanuRean <shantanu.parate@reancloud.com>
2018-10-31 17:43:36 +0530

    updated release to 0.0.7

--- REANPlatform/setup.py
+++ REANPlatform/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 PROJECT = 'REANPlatform'
 
 # Change docs/sphinx/conf.py too!
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 
 try:
     long_description = open('README.rst', 'rt').read()
