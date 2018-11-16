d22be70193b78b02ab9c6f15c248265adb186a40
Mayur Patil <mayur.patil@reancloud.com>
2018-11-16 16:25:30 +0530

    SDK added into setup.py

--- REANPlatform/setup.py
+++ REANPlatform/setup.py
@@ -38,7 +38,7 @@ setup(
 
     provides=[],
 
-    install_requires=['cliff', 'validators', 'boto3', 'wheel', 'pycryptodome', 'certifi', 'python-jenkins', 'setuptools>=40.4.1'],
+    install_requires=['cliff', 'validators', 'boto3', 'wheel', 'pycryptodome', 'certifi', 'python-jenkins', 'setuptools>=40.4.1', 'authnz_sdk_client', 'deploy_sdk_client', 'test_sdk_client'],
     namespace_packages=[],
     packages=find_packages(),
     include_package_data=True,
