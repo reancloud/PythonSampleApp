ad8c522f14fbf3a1d11dac9c228c457cb8bffe14
ShantanuRean <shantanu.parate@reancloud.com>
2018-11-22 16:18:18 +0530

    checks resolved

--- REANPlatform/deploy/getterraformcode.py
+++ REANPlatform/deploy/getterraformcode.py
@@ -2,15 +2,10 @@
 import os
 from pathlib import Path
 import logging
-import requests
-from reanplatform.constants import Constants
 from cliff.command import Command
-import deploy_sdk_client
 from deploy_sdk_client.rest import ApiException
-from reanplatform.set_header import set_header_parameter
+from reanplatform.constants import Constants
 from reanplatform.utility import Utility
-from deploy.constants import DeployConstants
-from deploy.utility import DeployUtility
 
 
 class GetTerraformCode(Command):
@@ -23,16 +18,16 @@ class GetTerraformCode(Command):
         # Define parser
         parser = super(GetTerraformCode, self).get_parser(prog_name)
         parser.add_argument('--env_id', '-i', help='Environment id', required=True)
-        parser.add_argument('--output_directory', '-o', help='Set Output directory to store terraform files.')
+        parser.add_argument('--output_directory', '-o',
+                            help='Set Output directory to store terraform files.')
         return parser
 
     def take_action(self, parsed_args):
         """take_action."""
         # Define parsed_args
         env_id = parsed_args.env_id
-
-        try:            
-            path = '/env/download/terraform/' + env_id   
+        try:
+            path = '/env/download/terraform/' + env_id
             curl_url = Constants.PLATFORM_BASE_URL + Constants.DEPLOY_URL + path
             api_response = Utility.get_zip_stream(curl_url)
             file_name = 'tf_files-' + env_id + '.tar.gz'
