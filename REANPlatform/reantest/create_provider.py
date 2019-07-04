984aaf3db4166c4e61f218d33041a8bca2cbefa1
mayur patil <mayurvpatil>
2019-07-04 17:10:52 +0530

    Parameters reordered as per UI and removed instance role arn which is removed from provider.

--- REANPlatform/reantest/create_provider.py
+++ REANPlatform/reantest/create_provider.py
@@ -16,17 +16,22 @@ class CreateProvider(Command):
         """get_parser."""
         parser = super(CreateProvider, self).get_parser(prog_name)
         parser.add_argument('--provider_name', '-n', help='Set provider name', required=True)
-        parser.add_argument('--provider_type', '-t', help='Set provider type. Example:AWS', required=True)
-        parser.add_argument('--provider_file_path', '-pd', help='Json file with applicable key-value pair for provider type. File absolute path', required=True)
-        parser.add_argument('--instance_profile_arn', '-a', help='Set Instance profile ARN', required=True)
-        parser.add_argument('--subnet_id', '-s', help='Set subnet Id', required=True)
-        parser.add_argument('--security_group', '-sg', help='Set security group', required=True)
-        parser.add_argument('--key_pair_name', '-k', help='Set kep pair name', required=True)
-        parser.add_argument('--bucket_name', '-b', help='Set Bucket name', required=True)
-        parser.add_argument('--instance_profile_role', '-r', help='Set Instance profile role', required=True)
-        parser.add_argument('--use_public_ip', '-p', help='Set true to use public ip, Default value is false', default=False)
-        parser.add_argument('--default_provider', '-d', help='Set true to set default provider, Default value is false', default=False)
+        parser.add_argument('--provider_type', '-t', help='Set provider type', choices=['aws'], required=True)
+        parser.add_argument('--provider_file_path', '-pd', help='Json file with applicable key-value pair for '
+                            'provider type. File absolute path', required=True)
+        parser.add_argument('--subnet_id', '-s', help='Subnet id to launch EC2 instances to perform testing',
+                            required=True)
+        parser.add_argument('--security_group', '-sg', help='Security Group Id to associate to launched EC2 instance',
+                            required=True)
+        parser.add_argument('--bucket_name', '-b', help='Bucket name to store REAN Test Report', required=True)
+        parser.add_argument('--instance_profile_name', '-a', help='Instance Profile Name is used to '
+                            'upload reports to s3', required=True)
+        parser.add_argument('--key_pair_name', '-k', help='Set key pair name', required=True)
         parser.add_argument('--private_key_file_path', '-pk', help='Private Key to connect to instance', required=True)
+        parser.add_argument('--use_public_ip', '-p', help='Is Use Public IP for connecting test instance',
+                            choices=['true', 'false'], default=False)
+        parser.add_argument('--default_provider', '-d', help='Set true to set default provider, Default value is false',
+                            choices=['true', 'false'], default=False)
         parser.add_argument('--output', '-o', help="Write output to <file> instead of stdout.", required=False)
         return parser
 
@@ -49,12 +54,11 @@ class CreateProvider(Command):
                 data=provider_details_json
             )
 
-            body.instance_profile_arn = parsed_args.instance_profile_arn
             body.subnet_id = parsed_args.subnet_id
             body.security_group_id = parsed_args.security_group
             body.key_pair_name = parsed_args.key_pair_name
             body.bucket_name = parsed_args.bucket_name
-            body.test_instance_role = parsed_args.instance_profile_role
+            body.test_instance_role = parsed_args.instance_profile_name
             body.use_public_ip = parsed_args.use_public_ip
             body.default_provider = parsed_args.default_provider
             body.private_key = private_key
@@ -66,8 +70,6 @@ class CreateProvider(Command):
             api_response = api_instance.save_provider(body)
 
             self.log.debug(api_response)
-
-            PlatformUtility.print_output_as_str("{}".format(api_response), parsed_args.output)
 
             print("Provider created successfully.")
         except Exception as exception:
