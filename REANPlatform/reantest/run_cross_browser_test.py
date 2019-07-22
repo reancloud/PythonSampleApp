32c6c6783d86570d60ed42e8a6720917db13a654
mayur patil <mayurvpatil>
2019-07-22 16:48:44 +0530

    cross browser cli updated as per new resource

--- REANPlatform/reantest/run_cross_browser_test.py
+++ REANPlatform/reantest/run_cross_browser_test.py
@@ -11,34 +11,47 @@ class RunCrossBrowserTest(Command):
 
     log = logging.getLogger(__name__)
 
+    _description = 'Run Cross browser Test'
+    _epilog = 'Example: rean-test run-automation-test --name <name> --test_suite Selenium --url <url> ' \
+              '--git_repository_url <git_url> --git_branch <branch> --command_to_run_test <command to run test> ' \
+              '--output_directory_path <report path> --report_file_name <report file name>  --chrome 64 ' \
+              '--automation_code_language <Java/Ruby>'
+
     def get_parser(self, prog_name):
         """get_parser."""
         parser = super(RunCrossBrowserTest, self).get_parser(prog_name)
 
-        parser.add_argument('--app_name', '-a',
+        # Basic configuration
+        parser.add_argument('--name', '-n',
                             help='Set the name for this Automation Job.',
                             required=True)
+        parser.add_argument('--test_suite', '-ts',
+                            choices=['Selenium', 'UFT', 'TestComplete'],
+                            help='Set test suite')
         parser.add_argument('--url', '-u',
                             help='Set url To be used in Automation test. Example:http://www.google.com.')
-        parser.add_argument('--code_file_name', '-cf',
-                            help='Set upload file name',
-                            default="test")
-        parser.add_argument('--git_repository_url', '-gr',
-                            help='Set git clone url for Automation code.')
-        parser.add_argument('--git_username', '-gu',
-                            help='Set user id in case of private git repository.')
-        parser.add_argument('--git_password', '-gp',
-                            help='Set user password in case of private git repository')
-        parser.add_argument('--git_branch_name', '-gb',
-                            help='Set git repository branch name. '
-                                 'If not specified, master branch will be considered by default.',
-                            default='master')
-        parser.add_argument('--command_to_run_test', '-rc',
-                            help='Set command to run Automation Testsuite. For e.g. mvn test ')
-        parser.add_argument('--automation_code_type', '-at',
-                            help='Set automation code type as Ruby, Java, VBScript ')
         parser.add_argument('--preserve_machine', '-p',
-                            help='Set true for preserve machine if test fails.')
+                            choices=['true', 'False'],
+                            help='Set true for preserve machine if test fails. default: False',
+                            default=False)
+        parser.add_argument('--automation_code_language', '-al',
+                            choices=['Ruby', 'Java', 'VBScript'],
+                            help='Set automation code type')
+
+        # CodeBase Parameters
+
+        parser.add_argument('--upload_code_file_name', '-cf', help='Set upload file name', default="test")
+
+        parser.add_argument('--git_repository_url', '-gr', help='Set git clone url for Automation code.')
+        parser.add_argument('--git_username', '-gu', help='Set git username for Automation code.')
+        parser.add_argument('--git_password', '-gp', help='Set git password for Automation code.')
+        parser.add_argument('--git_branch', '-gb',
+                            help='Set git repository branch name. '
+                                 'If not specified, master branch will be considered by default.', default='master')
+
+        # Execution Parameters
+        parser.add_argument('--command_to_run_test', '-rc',
+                            help='Set command to run Automation Testsuite. For e.g. mvn test')
         parser.add_argument('--pre_script', '-pr',
                             help='Set shell script to be executed before test suite runs.'
                                  'For e.g. mvn clean install to build your automation code.')
@@ -50,16 +63,13 @@ class RunCrossBrowserTest(Command):
                             help='Set test execution reports directory.'
                                  'Example target/testng-report,'
                                  'Path should be relative to your automation code directory.')
-        parser.add_argument('--delete_virtual_machine', '-d',
-                            help='Set delete vm as true or false')
-        parser.add_argument('--run_sequential', '-rs',
-                            help='Set run sequential as true or false')
+
+        # Sample Code Parameters
         parser.add_argument('--sample_code_type', '-ct',
                             choices=['Ruby', 'Java', 'HP-UFT', 'TestComplete'],
                             help='Set sample code type')
-        parser.add_argument('--test_suite', '-ts',
-                            choices=['Selenium', 'UFT', 'TestComplete', 'NA'],
-                            help='Set test suite')
+
+        # Browsers
 
         parser.add_argument('--chrome', '-c',
                             help='Give the comma separated versions for Chrome to run test on.')
@@ -67,6 +77,7 @@ class RunCrossBrowserTest(Command):
                             help='Give the comma separated versions for Firefox to run test on.')
         parser.add_argument('--ie', '-i',
                             help='Give the comma separated versions for IE to run test on.')
+        # Wait parameter
         parser.add_argument('--wait', '-w',
                             help='Set to true for wait until job to finish.')
 
@@ -83,45 +94,65 @@ class RunCrossBrowserTest(Command):
 
             RunCrossBrowserTest.validate_inputs(parsed_args)
 
-            body = test_sdk_client.CrossBrowserTestDto()
-            body.app_name = parsed_args.app_name
-            body.browser_list = browser_list
-            body.sample_code_type = parsed_args.sample_code_type
-            body.test_suite = parsed_args.test_suite
-            body.deletevm = parsed_args.delete_virtual_machine
-            body.run_sequential = parsed_args.run_sequential
-            body.type = "functionaltest"  # type
-            body.execution_strategy = "boost"
-            body.is_presrve_if_failed = parsed_args.preserve_machine
-            body.automation_code_type = parsed_args.automation_code_type
+            functional_test_dto = test_sdk_client.FunctionalTestDto()
+            functional_test_dto.name = parsed_args.name
+            functional_test_dto.browsers = browser_list
+            functional_test_dto.test_suite = parsed_args.test_suite
+            functional_test_dto.type = "functionaltest"  # type
+            functional_test_dto.preserve_machine = parsed_args.preserve_machine
 
             # Add hardcoded values for sample code
             if parsed_args.sample_code_type:
-                RunCrossBrowserTest.set_sample_parameters(parsed_args.sample_code_type, body)
+                RunCrossBrowserTest.set_sample_parameters(parsed_args.sample_code_type, functional_test_dto)
             else:
-                body.test_url = parsed_args.url
+                functional_test_dto.test_url = parsed_args.url
+                if parsed_args.test_suite == 'Selenium':
+                    functional_test_dto.automation_language = parsed_args.automation_code_language
 
-                if parsed_args.code_file_name != 'test':
+                if parsed_args.upload_code_file_name != 'test':
+                    functional_test_dto.codebase_type = 'UPLOAD_CODE'
                     self.log.debug("Uploading code file ...")
-                    body.code_file_name = Utility.upload_code(parsed_args.code_file_name, parsed_args.app_name)
-                    self.log.debug("Code object Name : %s ", body.code_file_name)
-                    body.use_code_upload = True
+                    functional_test_dto.upload_code_file_name = Utility.upload_code(parsed_args.upload_code_file_name,
+                                                                                    parsed_args.name)
+                    self.log.debug("Code object Name : %s ", parsed_args.upload_code_file_name)
                 else:
-                    body.git_url = parsed_args.git_repository_url
-                    body.git_pass = parsed_args.git_password
-                    body.git_user = parsed_args.git_username
-                    body.branch_name = parsed_args.git_branch_name
-                    body.use_code_upload = False
+                    functional_test_dto.codebase_type = 'GIT'
 
-                body.command_to_run_test = parsed_args.command_to_run_test
-                body.pre_script = parsed_args.pre_script
-                body.post_script = parsed_args.post_script
-                body.report_file = parsed_args.report_file_name
-                body.output_dir = parsed_args.output_directory_path
+                    git_config_dto = test_sdk_client.GitConfigDto()
+                    git_config_dto.url = parsed_args.git_repository_url
+                    git_config_dto.passsword = parsed_args.git_password
+                    git_config_dto.user = parsed_args.git_username
+                    git_config_dto.branch = parsed_args.git_branch
+                    functional_test_dto.git_config = git_config_dto
 
-            self.log.debug(body)
-            self.log.debug("Execution stared for Automation Test")
-            Utility.execute_test(body, parsed_args, self.log, test_sdk_client.RunJobsApi(Utility.set_headers()).submit_cross_browser_test_job)
+                execution_details_dto = test_sdk_client.ExecutionDetailsDto()
+                execution_details_dto.run_command = parsed_args.command_to_run_test
+                execution_details_dto.pre_script = parsed_args.pre_script
+                execution_details_dto.post_script = parsed_args.post_script
+                execution_details_dto.report_file = parsed_args.report_file_name
+                execution_details_dto.output_dir = parsed_args.output_directory_path
+                functional_test_dto.execution_details = execution_details_dto
+
+            self.log.debug(functional_test_dto)
+            self.log.debug("Execution stared for Cross Browser Test")
+            print("1")
+            type(functional_test_dto)
+
+            response_functional_test_dto = test_sdk_client.RunTestNewApi(Utility.set_headers()).run_functional_test(
+                functional_test_dto)
+            print("2")
+
+            job_id = ""
+            if response_functional_test_dto.id:
+                job_id = response_functional_test_dto.id
+
+            self.log.debug("Response is------------: %s ", job_id)
+            print("The request Cross browser test submitted successfully. Job Id is : ", job_id)
+
+            if job_id is not None and hasattr(parsed_args, 'wait') and parsed_args.wait == "true":
+                api_instance = test_sdk_client.RunTestApi(Utility.set_headers())
+                Utility.wait_while_job_running(api_instance, job_id)
+
         except Exception as exception:
             self.log.error(exception)
             Utility.print_exception(exception)
@@ -135,39 +166,42 @@ class RunCrossBrowserTest(Command):
 
         message = ""
         if params.sample_code_type:
-            if params.app_name is None:
-                message = "Please enter app_name parameters."
+            if params.name is None:
+                message = "Please enter name parameters."
             elif params.chrome is None and params.firefox is None and params.ie is None:
                 message = "Please Provide at least one browser to Test."
         else:
             # Validation for Test URL
             if not validators.url(params.url):
                 message = "Please enter valid Application URL."
-            elif params.app_name is None:
-                message = "Please enter app_name parameters."
+            elif params.name is None:
+                message = "Please enter name parameters."
             elif params.command_to_run_test is None:
                 message = "Please enter command_to_run_test parameters."
-            elif params.automation_code_type is None:
-                message = "Please enter automation_code_type parameters."
             elif params.report_file_name is None:
                 message = "Please enter report_file parameters."
             elif params.output_directory_path is None:
                 message = "Please enter output_dir parameters."
             elif params.test_suite is None:
                 message = "Please enter test_suite parameters."
+            elif params.test_suite == 'Selenium':
+                if not params.automation_code_language:
+                    message = "automation_code_language parameter is mandatory with Seleniun test suit."
+                elif params.automation_code_language != 'Ruby' and params.automation_code_language != 'Java':
+                    message = "Ruby and Java are supported options for selenium test suit."
 
             # Validation for Browser list
             elif params.chrome is None and params.firefox is None and params.ie is None:
                 message = "Please Provide at least one browser to Test."
 
-            if params.code_file_name == 'test':  # Upload Code = false
+            if params.upload_code_file_name == 'test':  # Upload Code = false
                 if params.git_repository_url is None:
                     message = "Please provide valid git credentials"
             else:
                 if params.git_repository_url is not None:
                     message = "Upload file name and Git repository url parameters can not be used together"
 
-            if message:
+        if message:
                 raise RuntimeError(message)
 
     @staticmethod
@@ -175,36 +209,61 @@ class RunCrossBrowserTest(Command):
         """Set body parameters for sample code."""
         if sample_code_type == "Ruby":
             body.test_url = "https://34.199.118.11/"
-            body.git_url = "https://github.com/reancloud/testnowrubyexample.git"
-            body.branch_name = "master"
-            body.command_to_run_test = "bundle install && cucumber features"
-            body.output_dir = "reports"
-            body.report_file = "magento_report.json"
+            body.automation_language = 'Ruby'
+
+            git_config_dto = test_sdk_client.GitConfigDto()
+            git_config_dto.url = "https://github.com/reancloud/testnowrubyexample.git"
+            git_config_dto.branch = "master"
+            body.git_config = git_config_dto
+
+            execution_details_dto = test_sdk_client.ExecutionDetailsDto()
+            execution_details_dto.run_command = "bundle install && cucumber features"
+            execution_details_dto.output_dir = "reports"
+            execution_details_dto.report_file = "magento_report.json"
+            body.execution_details = execution_details_dto
 
         elif sample_code_type == "Java":
             body.test_url = "https://34.199.118.11/"
-            body.git_url = "https://github.com/reancloud/testnowjavaexample.git"
-            body.branch_name = "master"
-            body.command_to_run_test = "java -jar magento-automation-1.0-tests.jar"
-            body.output_dir = "reports"
-            body.report_file = "index.json"
+            body.automation_language = 'Java'
+
+            git_config_dto = test_sdk_client.GitConfigDto()
+            git_config_dto.url = "https://github.com/reancloud/testnowjavaexample.git"
+            git_config_dto.branch = "master"
+            body.git_config = git_config_dto
+
+            execution_details_dto = test_sdk_client.ExecutionDetailsDto()
+            execution_details_dto.run_command = "java -jar magento-automation-1.0-tests.jar"
+            execution_details_dto.output_dir = "reports"
+            execution_details_dto.report_file = "index.json"
+            body.execution_details = execution_details_dto
 
         elif sample_code_type == "UFT":
             body.test_url = "https://34.199.118.11/"
-            body.git_url = "https://github.com/tahirstamboli/UFTMagentoTest"
-            body.branch_name = "master"
-            body.command_to_run_test = "Cscript 'C:/testnow/code/MagentoSuccessTest/RunUFT.vbs' 'C:/testnow/code/MagentoSuccessTest'"
-            body.output_dir = "Report"
-            body.report_file = "ResultDoc.dat"
+
+            git_config_dto = test_sdk_client.GitConfigDto()
+            git_config_dto.url = "https://github.com/tahirstamboli/UFTMagentoTest"
+            git_config_dto.branch = "master"
+            body.git_config = git_config_dto
+
+            execution_details_dto = test_sdk_client.ExecutionDetailsDto()
+            execution_details_dto.run_command = "Cscript 'C:/testnow/code/MagentoSuccessTest/RunUFT.vbs' 'C:/testnow/code/MagentoSuccessTest'"
+            execution_details_dto.output_dir = "Report"
+            execution_details_dto.report_file = "ResultDoc.dat"
+            body.execution_details = execution_details_dto
 
         elif sample_code_type == "TestComplete":
             body.test_url = "http://34.199.118.11/"
-            body.git_url = "https://github.com/rajashriDalavi/TestCompleteMagentoTest"
-            body.branch_name = "master"
-            body.command_to_run_test = """"C:\\Program Files (x86)\\SmartBear\\TestExecute 12\\Bin\\TestExecute.exe" "C:\\testnow\\code\\TestProject3\\TestProject3.pjs" /run /exit /SilentMode /exportLog:"C:\\testnow\\code\\Report\\Results.mht"
+
+            git_config_dto = test_sdk_client.GitConfigDto()
+            git_config_dto.url = "https://github.com/rajashriDalavi/TestCompleteMagentoTest"
+            git_config_dto.branch = "master"
+            body.git_config = git_config_dto
+
+            execution_details_dto = test_sdk_client.ExecutionDetailsDto()
+            execution_details_dto.run_command = """"C:\\Program Files (x86)\\SmartBear\\TestExecute 12\\Bin\\TestExecute.exe" "C:\\testnow\\code\\TestProject3\\TestProject3.pjs" /run /exit /SilentMode /exportLog:"C:\\testnow\\code\\Report\\Results.mht"
 if %errorlevel% neq 0 (set level=%errorlevel%)
  cd /d "c:\\testnow\\code\\TestProject3\\Log"
  for /d %%a in (*) do copy "%%a\\RootLogData.dat" "c:\\testnow\\code\\Report\\ResultDoc.dat";
  exit /b %level%"""
-            body.output_dir = "Report"
-            body.report_file = "ResultDoc.dat"
+            execution_details_dto.output_dir = "Report"
+            execution_details_dto.report_file = "ResultDoc.dat"
