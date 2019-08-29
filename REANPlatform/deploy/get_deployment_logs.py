cbd36dc75726b4da2b0d7ad38a327a03266bed0b
ShantanuRean <shantanu.parate@reancloud.com>
2019-08-29 12:22:26 +0530

    changes updated get_deployment_logs

--- REANPlatform/deploy/get_deployment_logs.py
+++ REANPlatform/deploy/get_deployment_logs.py
@@ -19,14 +19,15 @@ class GetDeploymentLogs(Command):
         """get_parser."""
         # Define parser
         parser = super(GetDeploymentLogs, self).get_parser(prog_name)
-        parser.add_argument('--deployment_id', '-di', help='Deployment id.', required=True)
+        parser.add_argument('--deployment_name', '-dn', default='default', help='Deployment name. Provide this attribute to get specific deployment else deployment name will be default', required=False)
+        parser.add_argument('--env_id', '-i', help='Environment id', required=True)
         parser.add_argument('--output', '-o',
                             help="Write output to <file> instead of stdout.",
                             required=False)
         return parser
 
     @staticmethod
-    def get_deployment_logs(deployment_id):
+    def get_deployment_logs(env_id, deployment_name):
         """Get Deployment Logs."""
         try:
             # Initialise api_response
@@ -35,7 +36,9 @@ class GetDeploymentLogs(Command):
             # Initialise api_client and api_instance to get deployment logs
             api_client = set_header_parameter(DeployUtility.create_api_client(), Utility.get_url(DeployConstants.DEPLOY_URL))
             api_instance = deploy_sdk_client.EnvironmentApi(api_client)
-            api_response = api_instance.get_deploy_resource_log(deployment_id, '-1')
+            # Get deployment
+            deployment = api_instance.get_all_deployments_for_environment_by_id_and_deployment_name(env_id, deployment_name)
+            api_response = api_instance.get_deploy_resource_log(deployment.id, '-1')
             return api_response
         except ApiException as api_exception:
             Utility.print_exception(api_exception)
@@ -43,11 +46,12 @@ class GetDeploymentLogs(Command):
     def take_action(self, parsed_args):
         """take_action."""
         # Define parsed arguments
-        deployment_id = parsed_args.deployment_id
+        env_id = parsed_args.env_id
+        deployment_name = parsed_args.deployment_name
         output = parsed_args.output
 
         # Get deployment logs
-        deployment_logs = GetDeploymentLogs.get_deployment_logs(deployment_id)
+        deployment_logs = GetDeploymentLogs.get_deployment_logs(env_id, deployment_name)
 
         if deployment_logs:
             if output is not None:
