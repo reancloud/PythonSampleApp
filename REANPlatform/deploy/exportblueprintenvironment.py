b675f41debeec71c6e2b4844d207bc5474f0df28
ShantanuRean <shantanu.parate@reancloud.com>
2019-12-16 13:41:38 +0530

    updated cli export-blueprint-environment

--- REANPlatform/deploy/exportblueprintenvironment.py
+++ REANPlatform/deploy/exportblueprintenvironment.py
@@ -4,8 +4,10 @@ import logging
 from cliff.command import Command
 import deploy_sdk_client
 from deploy_sdk_client.rest import ApiException
+from reanplatform.constants import Constants
 from reanplatform.set_header import set_header_parameter
 from reanplatform.utility import Utility
+from reanplatform.utilityconstants import PlatformConstants
 from deploy.constants import DeployConstants
 from deploy.utility import DeployUtility
 
@@ -41,15 +43,25 @@ class ExportBlueprintEnvironment(Command):
             # Initialise api_instance
             api_client = set_header_parameter(DeployUtility.create_api_client(), Utility.get_url(DeployConstants.DEPLOY_URL))
             api_instance = deploy_sdk_client.EnvironmentApi(api_client)
-            blueprint = api_instance.export_blueprint_environment(env_id)
             filename = blueprint_file_name
-
-            if filename is None:
-                response = api_instance.get_environment(env_id)
-                filename = response.name + '-' + response.env_version
-
-            blueprint_filepath = os.getcwd() + '/' + filename + '.blueprint.reandeploy'
-            Utility.create_output_file(blueprint_filepath, blueprint)
-            print("Blueprint file " + filename + " created successfully at " + blueprint_filepath)
+            path = '/env/export/blueprint/' + env_id
+            response = ExportBlueprintEnvironment.get_api_response(path)
+            if response is not None:
+                Utility.handleInvalidResponse(response, 200)
+                if filename is not None:
+                    output = filename + '.blueprint.reandeploy'
+                else:
+                    env = api_instance.get_environment(env_id)
+                    filename = env.name + '-' + env.env_version
+                    output = filename + '.blueprint.reandeploy'
+                Utility.print_output(Utility.get_parsed_serialized_json(response.content), output, PlatformConstants.STR_REFERENCE)
+                blueprint_filepath = os.getcwd() + '/' + filename + '.blueprint.reandeploy'
+                print("Blueprint file " + filename + " created successfully at " + blueprint_filepath)
         except ApiException as api_exception:
             Utility.print_exception(api_exception)
+
+    @staticmethod
+    def get_api_response(path):
+        """get_api_response."""
+        curl_url = Constants.PLATFORM_BASE_URL + Constants.DEPLOY_URL + path
+        return Utility.get_zip_stream(curl_url)
