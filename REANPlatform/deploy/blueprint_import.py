f8011629ba81fdf1e943961c992c5f8044a912e8
ShantanuRean <shantanu.parate@reancloud.com>
2020-06-24 13:31:54 +0530

    added auto share in import

--- REANPlatform/deploy/blueprint_import.py
+++ REANPlatform/deploy/blueprint_import.py
@@ -5,12 +5,15 @@ import logging
 import json
 import re
 from cliff.command import Command
+import authnz_sdk_client
+from auth.utility import AuthnzUtility
 import deploy_sdk_client
 from deploy_sdk_client.rest import ApiException
 from reanplatform.set_header import set_header_parameter
 from reanplatform.utility import Utility
 from deploy.constants import DeployConstants
 from deploy.utility import DeployUtility
+from deploy.share_entity import ShareEntity
 
 
 class ImportBlueprint(Command):
@@ -19,13 +22,15 @@ class ImportBlueprint(Command):
     log = logging.getLogger(__name__)
 
     # EPILog will get print after commands
-    _epilog = 'Example : rean-deploy import-blueprint --blueprint_file /Users/reandeploy/importEnvironment.blueprint.reandeploy --attribute_file /Users/reandeploy/import_blueprint_attributes.json'
+    _epilog = 'Example : rean-deploy import-blueprint --blueprint_file /Users/reandeploy/importEnvironment.blueprint.reandeploy --attribute_file /Users/reandeploy/import_blueprint_attributes.json --group_name dummyGroup --actions "VIEW, EDIT, DEPLOY"'
 
     def get_parser(self, prog_name):
         """get_parser."""
         parser = super(ImportBlueprint, self).get_parser(prog_name)
         parser.add_argument('--blueprint_file', '-b', help='Blueprint file. HCAP Deploy blueprint file path. A path can be absolute path.', required=True)
         parser.add_argument('--attribute_file', '-a', help='Blueprint attributes. HCAP Deploy blueprint attributes file path. A path can be absolute path.', required=True)
+        parser.add_argument('--group_name', '-gn', help='Group name. This parameter is to share environments with that specified group.', required=False)
+        parser.add_argument('--actions', '-p', help='Resource Actions. Sample value for this attribute is: "VIEW, DELETE, DEPLOY, EDIT, EXPORT".', required=False)
         parser.add_argument('--output', '-o', help="Write output to <file> instead of stdout.", required=False)
         return parser
 
@@ -33,19 +38,25 @@ class ImportBlueprint(Command):
         """take_action."""
         blueprint_path = parsed_args.blueprint_file
         attribute_path = parsed_args.attribute_file
+        group_name = parsed_args.group_name
+        actions = parsed_args.actions
 
-        ImportBlueprint.validate_parameters(blueprint_path, attribute_path)
+        ImportBlueprint.validate_parameters(blueprint_path, attribute_path, group_name, actions)
 
-        ImportBlueprint.blueprint_import(blueprint_path, attribute_path, parsed_args)    # noqa: E501
+        ImportBlueprint.blueprint_import(blueprint_path, attribute_path, group_name, actions, parsed_args)    # noqa: E501
 
     @staticmethod
-    def validate_parameters(blueprint_path, attribute_path):
+    def validate_parameters(blueprint_path, attribute_path, group_name, actions):
         """Validate cli parameters."""
         if blueprint_path is None and attribute_path is None:
             raise RuntimeError("Please provide HCAP Deploy blueprint file and attributes file absolute path")
+        if group_name is not None and actions is None:
+            raise RuntimeError("Please provider Resource Actions: --actions")
+        if actions is not None and group_name is None:
+            raise RuntimeError("Please provider Group name: --group_name")
 
     @staticmethod
-    def blueprint_import(blueprint_path, attribute_path, parsed_args):
+    def blueprint_import(blueprint_path, attribute_path, group_name, actions, parsed_args):
         """blueprint_import."""
         try:
             api_client = set_header_parameter(DeployUtility.create_api_client(), Utility.get_url(DeployConstants.DEPLOY_URL))
@@ -81,6 +92,10 @@ class ImportBlueprint(Command):
                                  location %s: " % (blueprint_all_env.environment_imports[index].import_config.name, attribute_path)
                             raise RuntimeError(re.sub(' +', ' ', exception_msg))
 
+            if group_name is not None and actions is not None:
+                api_instance_group = authnz_sdk_client.GroupControllerApi(AuthnzUtility.set_headers())
+                group = api_instance_group.get_group_with_name(group_name)
+                blueprint_all_env.share_policy = ShareEntity.get_share_policy('ENVIRONMENT', group, actions)
             api_env_instance.import_blueprint(body=blueprint_all_env)
             Utility.print_output_as_str("Blueprint imported successfully. Environment names : {}".format(env_names), parsed_args.output)
 
