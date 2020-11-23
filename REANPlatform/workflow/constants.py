b6978ae8027fd0a67f121690accae3fc15012aa5
GuptaAnubhavAmit <anubhav.gupta@reancloud.com>
2020-11-23 14:23:27 +0530

    deploy SP using CLI

---
+++ REANPlatform/workflow/constants.py
@@ -0,0 +1,17 @@
+"""Contains constats required for commands."""
+
+
+class WorkflowConstants:
+    """Contains constats required for CLI."""
+
+    WORKFLOW_URL = '/api/reanworkflowengine'
+    NAME_REFERENCE = 'name'
+    PROVIDER_DETAILS_REFERENCE = 'provider_details'
+    PROVIDER_TYPE_REFERENCE = 'type'
+
+    def __setattr__(self, attr, value):
+        """__setattr__."""
+        if hasattr(self, attr):
+            raise Exception("Attempting to alter read-only value")
+
+        self.__dict__[attr] = value
