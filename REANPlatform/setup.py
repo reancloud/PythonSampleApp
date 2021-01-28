c4c280a026ba633b32a3905e4a54bf75bb24b2c9
Ivneetwahi <47813650+Ivneetwahi@users.noreply.github.com>
2021-01-28 13:58:11 +0530

    Update setup.py

--- REANPlatform/setup.py
+++ REANPlatform/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '7.0.0' 
 DESCRIPTION = 'My first Python package'
 LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 
